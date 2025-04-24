820d29e855c68bd46f0287ba0afcb88c502f49e2
@Deelaw <lauraighwre@gmail.com>
2025-04-24 11:41:52 +0200

    implemented formular 24

--- src/stem_volumes/formulas.py
+++ src/stem_volumes/formulas.py
@@ -73,7 +73,7 @@ def stem_volume_formula_7():
     pass
 
 def stem_volume_formula_8(D, H):
-    
+    #coefficients
     a=0.00046903
     b=1.807
     c=0.0292
@@ -169,8 +169,15 @@ def stem_volume_formula_22():
 def stem_volume_formula_23():
     pass
 
-def stem_volume_formula_24():
-    pass
+def stem_volume_formula_24(D, H):
+     #coefficients
+    a=1.89060
+    b=0.26595
+    c=-1.07055
+    
+
+    V = D**a *H**b *math.exp(c)
+    return V
 
 def stem_volume_formula_25():
     pass
