5333adc5544db453c199155ff0ba2f8493e40972
Hannah Greven <hannah.greven@hnee.de>
2025-04-24 15:19:37 +0200

    Add test for formula #54

--- tests/test_formulas.py
+++ tests/test_formulas.py
@@ -248,7 +248,8 @@ def test_stem_volume_formula_53():
     assert 1==1
 
 def test_stem_volume_formula_54():
-    assert 1==1
+    assert stem_volume_formula_54(10, 2.5) > 0
+    assert stem_volume_formula_54(10, 2.5) < 10 # m³
 
 def test_stem_volume_formula_55():
     assert 1==1
