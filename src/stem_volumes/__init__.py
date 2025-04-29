b21fa83e35449659d05a0988e0892960724df908
Jens Mueller <jens.mueller@hnee.de>
2025-04-29 08:01:16 +0200

    Add command line parsing for script

--- src/stem_volumes/__init__.py
+++ src/stem_volumes/__init__.py
@@ -1,5 +1,23 @@
+import argparse
+import os
+import sys
+
 def main():
-    print("Hello World")
+    args = parse_arguments()
+
+    if os.path.exists(args.output_file):
+        print(f"Error: {args.output_file} already exists.", file=sys.stderr)
+        print(f"This script won't overwrite it to avoid accidental data loss.", file=sys.stderr)
+        sys.exit(1)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Calculate stem volumes for given CSV file"
+    )
+
+    parser.add_argument("csv_file", help="Path to CSV file, e.g., path/to/test.csv.")
+    parser.add_argument("output_file", help="Path to CSV file to save the added results in.")
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
